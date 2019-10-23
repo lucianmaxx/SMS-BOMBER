fe3f242bd90f356f3c02b57861bd974b679e8eaf
lucianmaxx <55327936+lucianmaxx@users.noreply.github.com>
2019-10-23 17:24:40 +0200

    Update JohnsTextBomber.py

--- JohnsTextBomber.py
+++ JohnsTextBomber.py
@@ -4,13 +4,13 @@ import smtplib
 
 #CONFIG. You can change any of the values on the right.
 email_provider = 'smtp.gmail.com' #server for your email- see ReadMe on github
-email_address = "YourEmail@gmail.com" #your email
+email_address = "kimivale146@gmail.com" #your email
 email_port = 587 #port for email server- see ReadMe on github
-password = "password123" #your email password
-msg = "Your message that you want sent to target" #your txt message
-text_amount = 20 #amount sent
+password = "Denisajepyca19" #your email password
+msg = "Je treba vysat znovu a poradne a potichu, poradne a potichu, potichu!!!" #your txt message
+text_amount = 30 #amount sent
 target_email = "5551234567@mms.att.net" #target number. must be in email form- see ReadMe on github
-wait = 1 #seconds in between messages
+wait = 30 #seconds in between messages
 #END CONFIG
 
 ### DO NOT EDIT BELOW THIS LINE ###
